61cdb5fdcf8662dbcab0b21b4218b2900cf0095a
hackeru <hackeru@hackeru4.local>
2017-01-05 18:09:50 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Students' });
+});
+
+
+router.get('/students', function(req, res, next) {
+  res.render('students', { title: 'Students' });
+});
+
+module.exports = router;

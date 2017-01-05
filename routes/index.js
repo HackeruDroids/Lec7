9d06c5a99c9c6cc685b148e1186d929510b91b09
hackeru <hackeru@hackeru4.local>
2017-01-05 20:53:01 +0200

    added add students from

--- routes/index.js
+++ routes/index.js
@@ -15,31 +15,68 @@ router.get('/students', function (req, res, next) {
   });
 });
 
-var pg = require('pg');
+/* GET Add Student page. */
+router.get('/addStudent', function (req, res, next) {
+  res.render('addStudent', {
+    title: 'Add Student'
+  });
+});
 
-/* GET Students page. */
-router.get('/initDb', function (req, res, next) {
-  pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+/* POST Add Student page. */
+router.post('/addStudent', function (req, res, next) {
+  //
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+
+   pg.connect(process.env.DATABASE_URL, function (err, client, done) {
     if (err) {
       return res.render('error', {
         error: err,
         message: err.message
       });
     }
-    var SQL = "CREATE TABLE Students(id SERIAL UNIQUE, firstName TEXT, lastName TEXT, email TEXT)";
-    client.query(SQL, function (err, result) {
+    var SQL = "INSERT INTO Students(firstName, lastName, email) VALUES($1, $2, $3)";
+    client.query(SQL,[firstName, lastName, email], function (err, result) {
       if (err) {
         return res.render('error', {
           error: err,
           message: err.message
         });
       }
-      res.render('index', {title:"succeesfully added students table"});
+      done();
+      res.render('index', {title:"succeesfully added student"});
     });
   });
-  res.render('index', {
-    title: 'Students'
+
+  res.render('addStudent', {
+    title: 'Add Student'
   });
 });
+
+var pg = require('pg');
+
+/* GET Students page. */
+//router.get('/initDb', function (req, res, next) {
+  // pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+  //   if (err) {
+  //     return res.render('error', {
+  //       error: err,
+  //       message: err.message
+  //     });
+  //   }
+  //   var SQL = "CREATE TABLE Students(id SERIAL UNIQUE, firstName TEXT, lastName TEXT, email TEXT)";
+  //   client.query(SQL, function (err, result) {
+  //     if (err) {
+  //       return res.render('error', {
+  //         error: err,
+  //         message: err.message
+  //       });
+  //     }
+  //     done();
+  //     res.render('index', {title:"succeesfully added students table"});
+  //   });
+  // });
+//});
 
 module.exports = router;
